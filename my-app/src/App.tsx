e1ac27e109d0b7c2866895e290ac88855de670c9
hubert.firek <hubert.firek@looksoft.pl>
2022-02-16 19:58:36 +0100

    almost done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { HomePage } from "./components/HomePage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <div className="cont">
+        <div className="stars">
+          <HomePage />
+        </div>
+      </div>
     </div>
   );
 }
--- my-app/src/components/HomePage/index.tsx
+++ my-app/src/components/HomePage/index.tsx
@@ -6,13 +6,43 @@ import {
   CardMedia,
   List,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { useState } from "react";
 import { useEffect } from "react";
+import styled from "styled-components";
 import { API_URL } from "../../globals";
+import { Character, Starship } from "../../types/types";
 
-export const HomePage = () => {
-  const [starships, setStarships] = useState([]);
+const HomePage = () => {
+  const [starships, setStarships] = useState<Starship[]>();
+  const [people, setPeople] = useState<Character[]>();
+  const [randomStar, setRandomStar] = useState(0);
+  const [randomStar2, setRandomStar2] = useState(1);
+  const [game, setGame] = useState(false);
+  const [multiplayer, setMultiplayer] = useState(false);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [playerTwoScore, setPlayerTwoScore] = useState(0);
+
+  const getRandomInt = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  const handleOnePlayerClick = () => {
+    setGame(true);
+    setRandomStar(getRandomInt(0, 6));
+    setRandomStar2(getRandomInt(1, 9));
+  };
+
+  const handleTwoPlayersClick = () => {
+    setGame(true);
+    setMultiplayer(true);
+    setRandomStar(getRandomInt(0, 6));
+    setRandomStar2(getRandomInt(1, 9));
+  };
+
   const fetchData = () => {
     fetch(`${API_URL}starships/`)
       .then((response) => {
@@ -20,39 +50,183 @@ export const HomePage = () => {
       })
       .then((data) => setStarships(data.results));
   };
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  const whoWins = () => {
+    if (starships !== undefined && game) {
+      if (multiplayer) {
+        if (
+          parseInt(starships[randomStar].crew) >
+          parseInt(starships[randomStar2].crew)
+        ) {
+          setPlayerScore(playerScore + 1);
+          return `Player 1 wins!`;
+        } else if (
+          parseInt(starships[randomStar].crew) <
+          parseInt(starships[randomStar2].crew)
+        ) {
+          setPlayerTwoScore(playerTwoScore + 1);
+          return `Player 2 wins!`;
+        } else {
+          return `Draw`;
+        }
+      } else {
+        if (
+          parseInt(starships[randomStar].crew) >
+          parseInt(starships[randomStar2].crew)
+        ) {
+          return `Starship ${starships[randomStar].name} wins!`;
+        } else if (
+          parseInt(starships[randomStar].crew) <
+          parseInt(starships[randomStar2].crew)
+        ) {
+          return `Starship ${starships[randomStar2].name} wins!`;
+        } else {
+          return `Draw`;
+        }
+      }
+    }
+  };
   return (
     <>
-      <h1>Starships: </h1>
-      <ul>
-        {starships.map((starship: any, index: number) => (
-          <li key={index}>{starship.name}</li>
-        ))}
-      </ul>
+      <h1>Minigame with Star Wars</h1>
+      <h1>Choose the number of players: </h1>
+      <Button size="medium" onClick={() => handleOnePlayerClick()}>
+        1 player
+      </Button>
+      <Button size="medium" onClick={() => handleTwoPlayersClick()}>
+        2 players
+      </Button>
+      <GameWrapper>
+        {game && (
+          <>
+            {starships !== undefined && (
+              <StarshipCard>
+                <h2>{starships[randomStar].name}</h2>
+                <p>{starships[randomStar].cargo_capacity}</p>
+                <p>{starships[randomStar].passengers}</p>
+                <p>{starships[randomStar].starship_class}</p>
+                <p>{starships[randomStar].max_atmosphering_speed}</p>
+                <p>{starships[randomStar].crew}</p>
+              </StarshipCard>
+            )}
 
-      {/* <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {starships !== undefined && (
+              <Card
+                sx={{
+                  minWidth: 375,
+                  backgroundColor: "transparent",
+                  border: "2px dashed yellow",
+                }}
+              >
+                <CardContent>
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    color="yellow"
+                    fontFamily={"Starjout"}
+                  >
+                    {starships[randomStar2].name}
+                  </Typography>
+                  <Typography
+                    sx={{ mb: 1.5 }}
+                    color="yellow"
+                    fontFamily={"Starjout"}
+                  >
+                    {starships[randomStar2].cargo_capacity}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="yellow"
+                    fontFamily={"Starjout"}
+                  >
+                    {starships[randomStar2].passengers}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="yellow"
+                    fontFamily={"Starjout"}
+                  >
+                    {starships[randomStar2].starship_class}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="yellow"
+                    fontFamily={"Starjout"}
+                  >
+                    {starships[randomStar2].max_atmosphering_speed}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="yellow"
+                    fontFamily={"Starjout"}
+                  >
+                    {starships[randomStar2].crew}
+                  </Typography>
+                </CardContent>
+              </Card>
+            )}
+          </>
+        )}
+      </GameWrapper>
+      {game && whoWins()}
+      {multiplayer && (
+        <>
+          <Typography
+            variant="h3"
+            component="div"
+            color="yellow"
+            fontFamily={"Starjout"}
+          >
+            Score:
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography
+            variant="h5"
+            component="div"
+            color="yellow"
+            fontFamily={"Starjout"}
+          >
+            Player1: {playerScore}
           </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card> */}
+          <Typography
+            variant="h5"
+            component="div"
+            color="yellow"
+            fontFamily={"Starjout"}
+          >
+            Player2: {playerTwoScore}
+          </Typography>
+        </>
+      )}
+      {game && (
+        <Button
+          size="medium"
+          onClick={() =>
+            multiplayer ? handleTwoPlayersClick() : handleOnePlayerClick()
+          }
+        >
+          Play again
+        </Button>
+      )}
     </>
   );
 };
+
+export default HomePage;
+
+const StarshipCard = styled.div`
+  border-radius: 6px;
+  border: 2px dashed yellow;
+  dispaly: flex;
+  min-width: 375px;
+  padding: 10px;
+`;
+
+const GameWrapper = styled.div`
+  display: flex;
+  justify-content:center;
+  gap 100px;
+  padding: 50px;`;
